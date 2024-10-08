93333044c6af64576635069a02a12d6cefd9d6d0
jmaxence <deathloadtv@gmail.com>
2024-10-08 14:34:20 +0200

    creation de données

--- TakItQuiz/initdb/TABLES.sql
+++ TakItQuiz/initdb/TABLES.sql
@@ -3,6 +3,7 @@ create table users
     id SERIAL PRIMARY KEY,
     first_name TEXT not null,
     last_name TEXT not null,
+    mail TEXT not null,
     isAdmin bool not null,
     quizPlayed int [] not null
 );
